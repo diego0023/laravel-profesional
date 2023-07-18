67193b7c2e759338b0fbb2b8652ec5a30a7fe0f7
diego0023 <diegogalan.0023@gmail.com>
2023-07-18 15:13:12 -0600

    rutas basica

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+/* Tipos de rutas
+ toda ruta lleva ('ruta', Controlador), se les puede agregar un nombre ->name('');
+ Route::view(); // atajo solo se usa para mostrar una ruta estatica
+ Route::get();
+ Route::post();
+ Route::put();
+ Route::delete();
+ Route::patch(); */
+
+ Route::view('/','landing.index')->name('index');
+ Route::view('/about','landing.about')->name('about');
+
