85aeeb69b9a02028b803d3dd587bed13950b0b45
diego0023 <diegogalan.0023@gmail.com>
2023-07-20 14:32:01 -0600

    morphOne

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,16 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class Post extends Model
+class Image extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
 
-    public function image(): MorphOne{
-        return $this->morphOne(Image::class, 'imageable');
+    public function imageable(): MorphTo{
+        return $this->morphTo();
     }
-
 }
