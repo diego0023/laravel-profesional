3c2946435bb9b3713ecc7f59133549edacc141c2
diego0023 <diegogalan.0023@gmail.com>
2023-07-20 14:35:21 -0600

    morph many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Post extends Model
@@ -12,8 +13,8 @@ class Post extends Model
 
     protected $guarded = [];
 
-    public function image(): MorphOne{
-        return $this->morphOne(Image::class, 'imageable');
+    public function image(): MorphMany{
+        return $this->morphMany(Image::class, 'imageable');
     }
 
 }
