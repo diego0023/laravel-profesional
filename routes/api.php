2e249a7582db26cc8409630e460f76a3816c38be
diego0023 <diegogalan.0023@gmail.com>
2023-07-24 13:13:02 -0600

    middleware directo

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use App\Http\Resources\UserResource;
@@ -17,8 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/post', PostController::class);
+/* Route::resource('/post', PostController::class);
 Route::get('/user', function(Request $request){
     return new UserResource(User::find(1));
-});
+}); */
 
+Route::middleware('example')->get('/', [ExampleController::class, 'index']);
+Route::get('/no-acces', [ExampleController::class, 'noAcces'] )->name('no-acces');
